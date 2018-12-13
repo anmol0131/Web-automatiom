9a480e60c09301482cf8be6eff11a479b3e2aa7a
Anmol Garg <anmolgarg0131@gmail.com>
2018-12-13 10:41:34 +0530

    lets see

--- src/main/java/com/spring/test/EmployeeController.java
+++ src/main/java/com/spring/test/EmployeeController.java
@@ -16,9 +16,9 @@ class EmployeeController {
         String pathValue3 = request.getParameter("ContactNumber");
         String html =
                 "<html>" +
-                "<h1> Emloyees detail values </h1>" +
+                "<h1> Emloyees detail table </h1>" +
                         "<h3 id=\"name\"> Name: " + pathValue1 + "</h3>" +
-                        "<h3 id=\"name\"> Name: " + pathValue1 +"qwertyuiopasdfghjk"+ "</h3>" +
+                        "<h3 id=\"name\"> Name: " + pathValue1 + "</h3>" +
                         "<h3 id=\"id\"> Id: " + pathValue2 + "</h3>" +
                         "<h3 id=\"num\"> ContactNumber: " + pathValue3 + "</h3>" +
                 "</html>";
